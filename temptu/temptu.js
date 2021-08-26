ccd69408cb72aa266788303b48c3a78a5a1d9c25
Puja singh <pujasingh261202@gmail.com>
2021-08-27 01:07:48 +0530

    main

--- temptu/temptu.js
+++ temptu/temptu.js
@@ -13,15 +13,15 @@ function closeNav() {
 }
 
 // For Sticky Navbar
-window.onscroll = function() { myFunction() };
+// window.onscroll = function() { myFunction() };
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
 
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
+// function myFunction() {
+//     if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky")
+//     } else {
+//         navbar.classList.remove("sticky");
+//     }
+// }
